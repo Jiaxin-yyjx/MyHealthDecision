315b074386a37dca40989175c57c43d64a800ea7
Jiaxin Yang <80301995+Jiaxin-yyjx@users.noreply.github.com>
2024-03-30 18:31:11 -0400

    members

--- flask-server/server.py
+++ flask-server/server.py
@@ -7,7 +7,7 @@ CORS(app)
 # Members API Route
 @app.route("/members")
 def members():
-    return {"members": ["Jiaxin Yang", "Quan Fang"]}
+    return {"members": ["Member1", "Member2", "Member3"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
